39c0052d14efdce3e4a47242f73e11d155154744
jinuu <jin@Jinus-MacBook-Pro.local>
2017-02-02 18:36:56 -0500

    checkpoint-9-events: Incomplete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,34 +1,20 @@
  var pointsArray = document.getElementsByClassName('point');
  
- var animatePoints = function(points) {
+ var revealPoint - function(point){
+        points.style.opacity = 1;
+        points.style.transform = "scaleX(1) translateY(0)";
+        points.style.msTransform = "scaleX(1) translateY(0)";
+        points.style.WebkitTransform = "scaleX(1) translateY(0)"; 
+  };   
 
+
+ var animatePoints = function(points) {
+    forEach(points,revealPoint);     
  
-                 var revealFirstPoint = function() {
-                     points[0].style.opacity = 1;
-                     points[0].style.transform = "scaleX(1) translateY(0)";
-                     points[0].style.msTransform = "scaleX(1) translateY(0)";
-                     points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealSecondPoint = function() {
-                     points[1].style.opacity = 1;
-                     points[1].style.transform = "scaleX(1) translateY(0)";
-                     points[1].style.msTransform = "scaleX(1) translateY(0)";
-                     points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealThirdPoint = function() {
-                     points[2].style.opacity = 1;
-                     points[2].style.transform = "scaleX(1) translateY(0)";
-                     points[2].style.msTransform = "scaleX(1) translateY(0)";
-                     points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 revealFirstPoint();
-                 revealSecondPoint();
-                 revealThirdPoint();
- 
-             };
+
+  };     
+
+
 
 window.onload = function() {
     
@@ -44,3 +30,7 @@ window.onload = function() {
           });
  }
             
+                             
+                             
+                             
+     
