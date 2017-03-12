7e595fe9a985abf457d358a9c2803c5cdfdfc9ea
jinuu <jin@jinus-mbp.attlocal.net>
2017-03-12 19:38:11 -0400

    checkpoint-20-Buzz: Completed with errors

--- scripts/landing.js
+++ scripts/landing.js
@@ -7,20 +7,22 @@ var animatePoints = function() {
             transform: 'scaleX(1) translateY(0)'
         });
 
-        $.each($('.point'), revealPoint);
-
     };
+    
+    $.each($('.point'), revealPoint);
+};
 
-    $(window).load(function() {
 
-        if ($(window).height() > 950) {
+$(window).load(function() {
+
+    if ($(window).height() > 950) {
+        animatePoints();
+    }
+
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    $(window).scroll(function(event) {
+        if ($(window).scrollTop() >= scrollDistance) {
             animatePoints();
         }
-
-        var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
-        $(window).scroll(function(event) {
-            if ($(window).scrollTop() >= scrollDistance) {
-                animatePoints();
-            }
-        });
     });
+});
